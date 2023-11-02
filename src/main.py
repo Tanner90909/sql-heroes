e7eb092683baa5e01da96f26c66c6f8b3746d4be
Tanner90909 <tmmcdaniel909@gmail.com>
2023-11-02 18:16:40 +0000

    MVP complete

--- src/main.py
+++ src/main.py
@@ -1,83 +1,26 @@
-from database.db_connection import execute_query
+from database.db_connection import execute_query, execute_modify
 
 # CREATE
 
 def create_new_pirate():
+    pirate_name = input("Enter pirates name: ")
+    pirate_about_me = input("Enter pirates about me: ")
+    pirate_biography = input("Enter pirates biography: ")
     query = """
         INSERT INTO
             pirates (name, about_me, biography)
         VALUES
-            (
-                'God Usopp',
-                'Crew mate of the Straw Hat Pirates',
-                'Once a liar-liar pants on fire, after Luffy came to Usopps village and rescued Kaya from the infamous Black Cat Pirates, Usopp became the fourth member of the Straw Hat Pirates. He specializes in long range fighting and disguises.'
-            );
-
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (7, 1, 1);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (7, 2, 1);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (7, 3, 1);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (7, 4, 2);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (7, 5, 2);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (7, 6, 2);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (1, 7, 1);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (2, 7, 1);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (3, 7, 1);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (4, 7, 2);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (5, 7, 2);
-        INSERT INTO
-            relationships (pirate1_id, pirate2_id, relationship_type_id)
-        VALUES
-            (6, 7, 2);
-
-        INSERT INTO
-            ability_types (name)
-        VALUES
-            ('Sniper King');
-
-        INSERT INTO
-            abilities (pirate_id, ability_type_id)
-        VALUES
-            (7, 8)
+            (%s, %s, %s);
             """
-    execute_query(query)
+    params = (pirate_name, pirate_about_me, pirate_biography)
+    execute_modify(query, params)
     print("New pirate entry successfully created.")
+
+create_new_pirate()
 
 # READ
 
-def select_all_pirates():
+def view_all_pirates():
     query = """
         SELECT * from pirates
     """
@@ -86,9 +29,35 @@ def select_all_pirates():
         print(item)
     return returned_items
 
+# view_all_pirates()
+
 # UPDATE
 
-def modify_pirate():
+def update_pirate_bio(pirate_name, new_biography):
     query = """
-        SELECT 
+        UPDATE pirates
+        SET biography = %s
+        WHERE name = %s;
             """
+    params = (new_biography, pirate_name)
+    execute_modify(query, params)
+    print(f"Pirate '{pirate_name}' bio has been updated")
+
+pirate_to_update = input("Enter pirate name to update their biography: ")
+biography_to_update = input("Enter your new biography: ")
+update_pirate_bio(pirate_to_update, biography_to_update)
+
+# DELETE
+
+def delete_pirate(pirate_name):
+    query = """
+        DELETE FROM pirates
+        WHERE name = %s;
+            """
+    params = (pirate_name,)
+    execute_modify(query, params)
+    print(f"Pirate '{pirate_name}' deleted successfully")
+
+pirate_to_delete = input("Enter pirate name to be deleted: ")
+delete_pirate(pirate_to_delete)
+
